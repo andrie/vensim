83c23a33c28578d1bd044aaf15fa2eae162a1cdc
andrie <apdevries@gmail.com>
2024-04-03 16:29:34 +0100

    Refactor and plot total users

--- app.py
+++ app.py
@@ -9,42 +9,55 @@ import plotly.graph_objects as go
 ui.page_opts(title="Bass diffusion model of new product adoption")
 
 with ui.sidebar():
-    ui.input_slider("marketing_effect", "Marketing effect:", 0, 0.01, 0.001, step=0.001)
-    ui.input_slider("word_of_mouth_effect", "Word of mouth effect", 0, 1, 0.025, step=0.025)
+    ui.input_slider("marketing_effect", "Marketing effect:", 0.001, 0.01, 0.001, step=0.001)
+    ui.input_slider("word_of_mouth_effect", "Word of mouth effect", 0, 0.25, 0.025, step=0.025)
+
+
+@reactive.calc
+def results():
+    model.set_components({
+        'marketing_effect' : input.marketing_effect(),
+        'word_of_mouth_effect'  : input.word_of_mouth_effect()
+    })
+    mod = model.run()
+    # mod.TIME = mod.`INITIAL TIME` + cumsum(mod.`TIME STEP`)
+    mod = mod.reset_index()
+    return mod
+
 
 with ui.card(full_screen=True):
     @render_plotly
-    def bassplot():
-        model.set_components({
-            'marketing_effect' : input.marketing_effect(),
-            'word_of_mouth_effect'  : input.word_of_mouth_effect()
-        })
-        results = model.run()
-        # results.TIME = results.`INITIAL TIME` + cumsum(results.`TIME STEP`)
-        results = results.reset_index()
+    def new_user_plot():
+        mod = results()
+        return create_plotly(mod, "New customers", "New users")
+    
+    @render_plotly
+    def total_user_plot():
+        mod = results()
+        return create_plotly(mod, "Customers", "Total users")
 
-        lineplot = go.Figure()
 
-        lineplot.add_scatter(
-            # data_frame=results,
-            x=results["time"],
-            y=results["New customers"],
-            # labels={"New customers": "Model Results"},
-            mode="lines",
-            name = "trial"
-        )
-        lineplot.add_scatter(
-            x=baseline["time"],
-            y=baseline["New customers"],
-            mode="lines",
-            name = "Baseline",
-        )
-        lineplot.update_layout(
-            title="New users",
-            xaxis_title="Time",
-            yaxis_title="New users",
-        )        
-        return lineplot
+def create_plotly(mod, col, title):
+    lineplot = go.Figure()
+    lineplot.add_scatter(
+        x=mod["time"],
+        y=mod[col],
+        mode="lines",
+        name = "Trial"
+    )
+    lineplot.add_scatter(
+        x=baseline["time"],
+        y=baseline[col],
+        mode="lines",
+        name = "Baseline",
+    )
+    lineplot.update_layout(
+        title=title,
+        xaxis_title="Time",
+        yaxis_title="New users",
+    )        
+    return lineplot
+
 
 
 model = pysd.load("bass-diffusion.py")
@@ -52,9 +65,9 @@ model.set_components({
     'marketing_effect' : 0.001,
     'word_of_mouth_effect'  : 0.025
 })
+
 baseline = model.run()
 baseline = baseline.reset_index()
-# baseline.TIME = baseline.`INITIAL TIME` + cumsum(baseline.`TIME STEP`)
 
 
 
