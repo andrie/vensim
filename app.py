7d36674076f3e79ee1bc3a37c8acfb56e8cc7da4
andrie <apdevries@gmail.com>
2024-04-05 09:43:07 +0100

    Add total users and UI controls

--- app.py
+++ app.py
@@ -8,9 +8,15 @@ import plotly.graph_objects as go
 
 ui.page_opts(title="Bass diffusion model of new product adoption")
 
+defaults = {"me" : 0.001, "wom" : 0.02}
+
 with ui.sidebar():
-    ui.input_slider("marketing_effect", "Marketing effect:", 0.001, 0.01, 0.001, step=0.001)
-    ui.input_slider("word_of_mouth_effect", "Word of mouth effect", 0, 0.25, 0.025, step=0.025)
+    ui.input_slider("marketing_effect", "Marketing effect:", 
+                    0.001, 0.01, value = defaults["me"], step=0.001)
+    ui.input_slider("word_of_mouth_effect", "Word of mouth effect", 
+                    0, 0.2, value = defaults["wom"], step=0.01)
+    ui.input_action_button("btn_reset", "Reset defaults")
+    ui.input_checkbox("show_baseline", "Show baseline", False)
 
 
 @reactive.calc
@@ -20,21 +26,30 @@ def results():
         'word_of_mouth_effect'  : input.word_of_mouth_effect()
     })
     mod = model.run()
-    # mod.TIME = mod.`INITIAL TIME` + cumsum(mod.`TIME STEP`)
     mod = mod.reset_index()
     return mod
 
+@reactive.effect
+@reactive.event(input.btn_reset)
+def _():
+    ui.update_slider("marketing_effect", value = defaults["me"])
+    ui.update_slider("word_of_mouth_effect", value = defaults["wom"])
 
-with ui.card(full_screen=True):
-    @render_plotly
-    def new_user_plot():
-        mod = results()
-        return create_plotly(mod, "New customers", "New users")
-    
+
+card_height = 300
+
+# with ui.card(full_screen=True):
+with ui.card(height = card_height):
     @render_plotly
     def total_user_plot():
         mod = results()
         return create_plotly(mod, "Customers", "Total users")
+
+with ui.card(height = card_height):
+    @render_plotly
+    def new_user_plot():
+        mod = results()
+        return create_plotly(mod, "New customers", "New users")
 
 
 def create_plotly(mod, col, title):
@@ -45,16 +60,17 @@ def create_plotly(mod, col, title):
         mode="lines",
         name = "Trial"
     )
-    lineplot.add_scatter(
-        x=baseline["time"],
-        y=baseline[col],
-        mode="lines",
-        name = "Baseline",
-    )
+    if input.show_baseline():
+        lineplot.add_scatter(
+            x=baseline["time"],
+            y=baseline[col],
+            mode="lines",
+            name = "Baseline",
+        )
     lineplot.update_layout(
         title=title,
         xaxis_title="Time",
-        yaxis_title="New users",
+        yaxis_title=title,
     )        
     return lineplot
 
@@ -62,8 +78,8 @@ def create_plotly(mod, col, title):
 
 model = pysd.load("bass-diffusion.py")
 model.set_components({
-    'marketing_effect' : 0.001,
-    'word_of_mouth_effect'  : 0.025
+    'marketing_effect' : defaults["me"],
+    'word_of_mouth_effect'  : defaults["wom"]
 })
 
 baseline = model.run()
